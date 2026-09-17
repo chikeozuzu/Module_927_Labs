868ea41624e0e1f5ba5c18d85076e54c9f6b89da
chikeozuzu <chikeozuzu@gmail.com>
2026-09-16 20:48:59 -0400

    Completed Task 1 of GLAB_927_1_2

---
+++ GLAB_927_1_2.py
@@ -0,0 +1,8 @@
+#importing NLTK library
+import nltk
+
+#checking NLTK version
+nltk_version = nltk.__version__
+
+#print confirmation message and NLTK version
+print(f"NLTK library imported successfully! Version: {nltk_version}")
