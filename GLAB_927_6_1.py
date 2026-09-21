f94a31e6a6de49d263d3e41d646dc2ba1b3f24ca
chikeozuzu <chikeozuzu@gmail.com>
2026-09-21 17:22:11 -0400

    Completed Task 2 of GLAB_927_6_1 Lab

--- GLAB_927_6_1.py
+++ GLAB_927_6_1.py
@@ -4,3 +4,22 @@
 
 # Task 2: Setting up LangChain and LangChain-Groq
 
+import os
+from langchain_groq import ChatGroq
+
+#Assigning the Groq API key to an environment variable
+os.environ["GROQ_API_KEY"] = "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"
+
+#Initialize LangChain with Groq Llama LLM
+llm = ChatGroq(
+    model= "llama3-8b-8192"
+    )
+
+#Create a prompt template for FAQs
+messages = [
+    ("system", "You are a customer support representative. Your goal is to efficiently handle queries."),
+    ("human", "What are your business hours?"),
+]
+result = llm.invoke(messages)
+print(result.content)
+
