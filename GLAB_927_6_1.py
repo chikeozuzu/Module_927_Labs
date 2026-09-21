c718eaae2c24dbcc60fe090eb8d741d064347afc
chikeozuzu <chikeozuzu@gmail.com>
2026-09-21 17:11:11 -0400

    Started Task 2 of GLAB_927_6_1 Lab

--- GLAB_927_6_1.py
+++ GLAB_927_6_1.py
@@ -1,4 +1,6 @@
 # Task 1: Retrieving the Groq API Key
 
-# groq account and API key created and retrieved successfully. The API key is stored in the variable 'Chike_Groq_API'.
+# groq account and API key created and retrieved successfully. The API key is saved with the display name 'Chike_Groq_API'.
+
+# Task 2: Setting up LangChain and LangChain-Groq
 
